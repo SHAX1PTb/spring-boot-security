dccf7647d6859ea4d6976e40ea6aa902ae9b6d4b
Shankargouda Annigeri <shankar.annigeri@outlook.com>
2021-12-24 15:55:00 -0700

    GoldmanSachs

--- src/main/java/GoldmanSachs.java
+++ src/main/java/GoldmanSachs.java
@@ -3,38 +3,46 @@ import java.util.Map;
 
 public class GoldmanSachs {
 
-  public Integer maxAvgCalculator(String[][] student) {
+    public Integer maxAvgCalculator(String[][] student) {
 
-    //    System.out.println(student[0][1]);
-    //    System.out.println(student.length);
+        //    System.out.println(student[0][1]);
+        //    System.out.println(student.length);
 
-    Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
 
-    Integer a = 0;
+        Integer a = 0;
 
-    Integer count = 0;
-    Integer sum = 0;
-    for (int i = 0; i < student.length; i++) {
+        Integer count = 0;
+        Integer sum = 0;
+        for (int i = 0; i < student.length; i++) {
 
-      if (map.get(student[i][0]) == null) {
-        count = 1;
-        a = Integer.valueOf(student[i][1]);
-        //        count++;
-      } else {
-        count++;
-        a = (map.get(student[i][0]) + Integer.valueOf(student[i][1])) / count;
-      }
+            if (map.get(student[i][0]) == null) {
+                count = 1;
+                a = Integer.valueOf(student[i][1]);
+                //        count++;
+            } else {
+                count++;
+                a = (map.get(student[i][0]) + Integer.valueOf(student[i][1])) / count;
+            }
 
-      map.put(student[i][0], a);
+            map.put(student[i][0], a);
+        }
+
+        var b = map.getOrDefault(student[1][0], 0) + Integer.valueOf(student[1][1]);
+        System.out.println("var:" + b);
+
+        for (int i = 0; i < map.size(); i++) {
+            sum = sum + map.get(student[i][0]);
+        }
+
+        var enteries = map.entrySet();
+
+        for (Map.Entry<String, Integer> e : enteries) {
+            System.out.println(e.getValue());
+        }
+//    System.out.println(map.values());
+//    System.out.println(sum);
+
+        return sum / map.size();
     }
-
-    for (int i = 0; i < map.size(); i++) {
-      sum = sum + map.get(student[i][0]);
-    }
-
-    System.out.println(map.values());
-    System.out.println(sum);
-
-    return sum / map.size();
-  }
 }
