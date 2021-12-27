55692de332ddd8786a3cc41241ae021fe81967f6
SHAX1PTb03 <shankargouda.annigeri@capgemini.com>
2021-12-27 11:55:12 -0700

    Refactor the package name

--- src/main/java/com/practise/Company/mediumSecond.java
+++ src/main/java/com/practise/Company/mediumSecond.java
@@ -3,22 +3,22 @@ package com.practise.Company;
 import com.jsoniter.JsonIterator;
 import lombok.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Setter
 @Getter
 @NoArgsConstructor
 @ToString
 @AllArgsConstructor
-class Paragraph {
-  String id;
-  String text;
+class Paragraph2 {
+    String id;
+    String text;
 
-  @Override
-  public String toString() {
-    return "{" + "id:" + id + ", text:" + text + "}";
-  }
+    @Override
+    public String toString() {
+        return "{" + "id:" + id + ", text:" + text + "}";
+    }
 }
 
 @Setter
@@ -26,14 +26,14 @@ class Paragraph {
 @NoArgsConstructor
 @ToString
 @AllArgsConstructor
-class Section {
-  String id;
-  int startIndex;
+class Section2 {
+    String id;
+    int startIndex;
 
-  @Override
-  public String toString() {
-    return "{" + "id='" + id + '\'' + ", startIndex=" + startIndex + '}';
-  }
+    @Override
+    public String toString() {
+        return "{" + "id='" + id + '\'' + ", startIndex=" + startIndex + '}';
+    }
 }
 
 @Setter
@@ -41,77 +41,116 @@ class Section {
 @NoArgsConstructor
 @ToString
 @AllArgsConstructor
-class PostContent {
-  Paragraph[] paragraphs;
-  Section[] sections;
+class PostContent2 {
+    Paragraph[] paragraphs;
+    Section[] sections;
 
-  //    @Override
-  //    public String toString() {
-  //        return "PostContent{" +
-  //                "paragraphs=" + Arrays.toString(paragraphs) +
-  //                ", sections=" + Arrays.toString(sections) +
-  //                '}';
-  //    }
+    //    @Override
+    //    public String toString() {
+    //        return "PostContent{" +
+    //                "paragraphs=" + Arrays.toString(paragraphs) +
+    //                ", sections=" + Arrays.toString(sections) +
+    //                '}';
+    //    }
 }
+
+@Setter
+@Getter
+@NoArgsConstructor
+@ToString
+@AllArgsConstructor
 class Delta {
-  String type;
-  int paragraphIndex;
-  Paragraph paragraph;
+    String type;
+    int paragraphIndex;
+    Paragraph paragraph;
 }
 
-class Solution {
-  public static String solution(String postContentString) {
+class Solution2 {
+    public static String solution(String postContentString, String deltasString) throws InterruptedException {
 
-    var message1 = postContentString.split( "postContentString:");
-    System.out.println(message1[1]);
+        var message1 = postContentString.split("postContentString:");
+        System.out.println(message1[1]);
 
-    PostContent postContent = JsonIterator.deserialize(message1[1], PostContent.class);
+        PostContent postContent = JsonIterator.deserialize(message1[1], PostContent.class);
+        Delta[] deltas = JsonIterator.deserialize(deltasString, Delta[].class);
 
-    List<Section> s = new ArrayList<>();
-    List<Paragraph> p = new ArrayList<>();
-    List<Integer> integers = new ArrayList<>();
+        List<Section> s = new ArrayList<>();
+        List<Paragraph> p = new ArrayList<>();
+        List<Integer> integers = new ArrayList<>();
 
-    StringBuilder ss = new StringBuilder();
+        StringBuilder ss = new StringBuilder();
+        //Map<Integer,String> deltaUpdate = new LinkedHashMap<>();
+        List<String> deltaUpdate = new ArrayList<>();
 
-    for (int i = 0; i < postContent.sections.length; i++) {
-      s.add(postContent.sections[i]);
+        for (int i = 0; i < postContent.sections.length; i++) {
+            s.add(postContent.sections[i]);
+        }
+
+        for (int i = 0; i < postContent.paragraphs.length; i++) {
+            p.add(postContent.paragraphs[i]);
+        }
+
+        for (int i = 0; i < s.size(); i++) {
+
+            integers.add(s.get(i).startIndex);
+        }
+        int k = 0;
+
+        for (int i = 0; i < integers.size() - 1; i++) {
+
+            while (k < integers.get(i + 1)) {
+                // System.out.println(p.get(k).getText());
+                ss.append(p.get(k).getText());
+                deltaUpdate.add(p.get(k).getText());
+                ss.append("\n");
+                deltaUpdate.add("\\n");
+                k++;
+            }
+            //  System.out.println("---");
+            ss.append("-");
+            deltaUpdate.add("-");
+            ss.append("\n");
+            deltaUpdate.add("\\n");
+        }
+
+        for (int z = k; z < p.size(); z++) {
+
+            //   System.out.println("z"+(p.size()-1));
+            //     System.out.println(p.get(z).getText());
+            //  System.out.println(p.get(z).text+"\n");
+            ss.append(p.get(z).text);
+            deltaUpdate.add(p.get(z).text);
+            if (z != (p.size() - 1)) {
+                ss.append("\n");
+                deltaUpdate.add("\\n");
+            }
+        }
+
+        System.out.println(deltaUpdate.toString());
+
+
+        for (int i = 0; i < deltas.length; i++) {
+
+            // System.out.println(deltas[i].getType());
+            String action = deltas[i].getType();
+
+            switch (action) {
+
+                case "updateParagraph":
+                case "addParagraph":
+                    deltaUpdate.add(deltas[i].getParagraphIndex(), deltas[i].getParagraph().getText());
+                    break;
+
+                case "deleteParagraph":
+                    deltaUpdate.remove(deltas[i].getParagraphIndex());
+                    break;
+
+            }
+
+            System.out.println(deltaUpdate.toString());
+        }
+
+
+        return ss.toString();
     }
-
-    for (int i = 0; i < postContent.paragraphs.length; i++) {
-      p.add(postContent.paragraphs[i]);
-    }
-
-    for (int i = 0; i < s.size(); i++) {
-
-      integers.add(s.get(i).startIndex);
-    }
-    int k = 0;
-
-    for (int i = 0; i < integers.size() - 1; i++) {
-
-      while (k < integers.get(i + 1)) {
-       // System.out.println(p.get(k).getText());
-        ss.append(p.get(k).getText());
-        ss.append("\n");
-        k++;
-      }
-    //  System.out.println("---");
-      ss.append("-");
-      ss.append("\n");
-    }
-
-    for (int z = k; z < p.size(); z++) {
-
-   //   System.out.println("z"+(p.size()-1));
- //     System.out.println(p.get(z).getText());
-    //  System.out.println(p.get(z).text+"\n");
-      ss.append(p.get(z).text);
-      if(z !=(p.size()-1)){
-        ss.append("\n");
-      }
-    }
-
-    System.out.println(ss.toString());
-    return ss.toString();
-  }
 }
---
+++ src/test/java/com/practise/Company/Solution2Test.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2021.
+ * Shankargouda Annigeri
+ *
+ *
+ */
+
+package com.practise.Company;
+
+
+import org.junit.jupiter.api.Test;
+
+public class Solution2Test {
+
+    @Test
+    public void deltaTest() throws InterruptedException {
+        var message = "postContentString:{\"paragraphs\":[{\"id\":\"p0\",\"text\":\"aaa\"},{\"id\":\"p1\",\"text\":\"bbb\"},{\"id\":\"p2\",\"text\":\"ccc\"},{\"id\":\"p3\",\"text\":\"ddd\"},{\"id\":\"p4\",\"text\":\"eee\"},{\"id\":\"p5\",\"text\":\"fff\"}],\"sections\":[{\"startIndex\":0,\"id\":\"s0\"},{\"startIndex\":2,\"id\":\"s1\"},{\"startIndex\":4,\"id\":\"s2\"}]}";
+        var deltas = "[{\n" +
+                "\t\"type\": \"updateParagraph\",\n" +
+                "\t\"paragraphIndex\": 0,\n" +
+                "\t\"paragraph\": {\n" +
+                "\t\t\"text\": \"aaaAAA\"\n" +
+                "\t}\n" +
+                "}]";
+
+        var deltasAdd = "[{\n" +
+                "\t\"type\": \"addParagraph\",\n" +
+                "\t\"paragraphIndex\": 1,\n" +
+                "\t\"paragraph\": {\n" +
+                "\t\t\"text\": \"aaaAAA\"\n" +
+                "\t}\n" +
+                "}]";
+
+        var deltasDelete = "[{\n" +
+                "\t\"type\": \"deleteParagraph\",\n" +
+                "\t\"paragraphIndex\": 5,\n" +
+                "\t\"paragraph\": {\n" +
+                "\t\t\"text\": \"aaaAAA\"\n" +
+                "\t}\n" +
+                "}]";
+        Solution2.solution(message,deltasDelete);
+
+    }
+
+
+}
